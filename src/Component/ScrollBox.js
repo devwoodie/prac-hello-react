f77b0f6838e2cc3d16e0078ea061c703c4b1ec96
dwyu <ehddl453@naver.com>
2022-08-30 13:18:15 +0900

    feat : commit contribution

--- src/Component/ScrollBox.js
+++ src/Component/ScrollBox.js
@@ -9,7 +9,6 @@ class ScrollBox extends Component {
     render(){
         const style = {
             border: '1px solid black',
-
             height: '300px',
             width: '300px',
             overflow: 'auto',
