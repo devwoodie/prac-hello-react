07fd465900a1cf3c6c9ff56db25d805c6df11e10
devwoodie <ehddl453@naver.com>
2022-09-08 14:58:52 +0900

    feat : study useMemo, useCallback, useRef

--- src/Component/App.js
+++ src/Component/App.js
@@ -1,12 +1,13 @@
 import Info from "./Info";
 import {useState} from "react";
 import Counter2 from "./Counter2";
+import Average from "./Average";
 
 const App = () => {
     let [visible, setVisible] = useState(false);
 
     return (
-        <Info />
+        <Average />
     )
 }
 
